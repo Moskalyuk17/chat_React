e84afa2abd2e2f3b513e88b9f70806fff731905d
Moskalyuk17 <mamaliuk06@mail.ru>
2024-10-07 12:25:45 +0300

    first

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from 'react';
+import './Scss/index.css';
+import Aside from './Componets/Aside';
 
 function App() {
   return (
     <div className="App">
-      
+      <Aside /> 
     </div>
   );
 }
--- chat/src/Componets/Aside.js
+++ chat/src/Componets/Aside.js
@@ -1,11 +1,21 @@
 import React from "react";
+import newChat from "../Assect/addNewAssistante.svg";
+import closeSidebar from "../Assect/close_sidebar.svg";
 
 class Aside extends React.Component {
     render() {
         return (
-            <div className="aside">
-                
-            </div>
+            <aside className="aside">
+                <div className="container">
+                    <div className="header_aside">
+                        <button className="new_chat" data="Добавить нового ассистента"><img src={newChat} alt="Новый чат" /></button>
+                        <button className="close_sidebar" data="Закрыть боковую панель"><img src={closeSidebar} alt="Закрыть боковую панель" /></button>
+                    </div>
+                    <ul className="list_chat"></ul>
+                </div>
+            </aside>
         )
     }
 }
+
+export default Aside;
